8b182998a07d1a3e2d22a062e6e8b2652430aefe
nicopanico <nicopanico@hotmail.it>
2024-12-17 16:00:48 +0100

    Folders cleaned

--- Functions/calcola_numero_fotoni_raggiX.m
+++
@@ -1,5 +0,0 @@
-function N_fotoni = calcola_numero_fotoni_raggiX(kVp, mAs, distanza)
-    % Calcola il numero totale di fotoni per raggi X diagnostici
-    N_fotoni_base = kVp^2 * mAs;  % Numero di fotoni approssimato
-    N_fotoni = N_fotoni_base / (distanza^2);  % Fluence considerando la distanza
-end
